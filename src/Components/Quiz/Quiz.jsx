3e4d7c95d5ed73c83659d894d495e423b8b72590
SuryaHalder <hsurya2005@gmail.com>
2025-02-20 01:50:32 +0530

    FirstCommit

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,36 @@
+import React,{useState} from 'react'
+import './Quiz.css'
+import { data } from '../../assets/data';
+const Quiz = () => {
+    let [index,setIndex]=useState(1);
+    let [question,setQuestion]=useState(data[index]);
+    let [lock,setLock]=useState(false);
+    const checkAns=(e,ans)=>{
+        if(lock===false){
+            if(question.ans===ans){
+                e.target.classList.add("correct");    
+            }
+            else{
+                e.target.classList.add("wrong");
+            }
+            setLock(true);           
+        }   
+    }
+  return (
+    <div className='container'>
+      <h1>Quiz app</h1>
+      <hr />
+      <h2>{index+1}. {question.question}</h2>
+      <ul>
+        <li onClick={(e)=>checkAns(e,1)}>{question.option1}</li>
+        <li onClick={(e)=>checkAns(e,2)}>{question.option2}</li>
+        <li onClick={(e)=>checkAns(e,3)}>{question.option3}</li>
+        <li onClick={(e)=>checkAns(e,4)}>{question.option4}</li>
+      </ul>
+      <button>Next</button>
+    <div className="index">1 of 5 questions</div>
+    </div>
+  )
+}
+
+export default Quiz
